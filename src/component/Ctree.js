1ac4207ccac139f9adceec41bc8bef24c628d6fc
larry <larry@qq.com>
2022-12-17 23:40:44 +0800

    init

---
+++ src/component/Ctree.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Ctree.css'
+
+
+function Ctree(params) {
+    return (
+        <div className='Tree'>
+            <div class="star-container">
+                <div class="star"></div>
+            </div>
+            <div class="tree1"></div>
+            <div class="tree2"></div>
+            <div class="tree3"></div>
+            <div class="tree4"></div>
+            <div class="trunk"></div>
+            <div class="ornament-container1">
+                <div class="ornament"></div>
+            </div>
+            <div class="ornament-container2">
+                <div class="ornament"></div>
+            </div>
+            <div class="ornament-container3">
+                <div class="ornament"></div>
+            </div>
+            <div class="light1"></div>
+            <div class="light2"></div>
+            <div class="light3"></div>
+            <div class="light4"></div>
+            <div class="light5"></div>
+            <div class="light6"></div>
+            <div class="light7"></div>
+            <div class="light8"></div>
+            <div class="light9"></div>
+            <div class="light10"></div>
+        </div>
+    )
+}
+
+export default Ctree;
